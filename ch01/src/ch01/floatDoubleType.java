c4111fc5b56e6aafb4ed386721ab719b322adf31
Your Name <you@example.com>
2022-06-22 09:45:40 +0900

    ch01

---
+++ ch01/src/ch01/floatDoubleType.java
@@ -0,0 +1,17 @@
+package ch01;
+
+public class floatDoubleType {
+
+	public static void main(String[] args) {
+		// 실수 자료형
+		//Double을 표준으로 사용
+		//float 정밀도는 소수 6자리 까지, Double 16자리까지 가능
+		float FNum = 1.2345678F;
+		double dNum = 1.2345678912345678;
+		
+		System.out.println(FNum);
+		System.out.println(dNum);
+
+	}
+
+}
