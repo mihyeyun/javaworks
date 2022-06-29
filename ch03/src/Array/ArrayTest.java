863e20c6a22a13a9fb38d6e87b41daea68856125
mihyeyun <ymh___2761@naver.com>
2022-06-29 10:28:32 +0900

    ch03_teacher

---
+++ ch03/src/Array/ArrayTest.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+		//숫자를 5개 저장할 배열
+		int[] num = new int[5];
+		
+		//문자열을 3개 저장할 배열
+		String[] str = new String[3];
+		
+		//제품을 3개 저장할 배열
+		Product[] product = new Product[3];
+
+	}
+
+}
---
+++ ch03/src/Array/Product.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class Product {
+	//필드
+	String productName;
+	int price;
+	
+	public Product(String productName, int price) {
+		this.productName = productName;
+		this.price = price;
+	};
+}
