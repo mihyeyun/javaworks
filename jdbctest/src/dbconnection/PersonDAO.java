b93569a7e4f9d427e9bb7eea7bd64754672a3a9f
mihyeyun <ymh___2761@naver.com>
2022-07-11 17:55:39 +0900

    db

--- jdbctest/src/dbconnection/PersonDAO.java
+++ jdbctest/src/dbconnection/PersonDAO.java
@@ -62,9 +62,63 @@ public class PersonDAO {
 	
 	
 	//자료 수정
+	public void updatePerson(Person person) {
+		conn = JDBCUtil.getConnection();
+		String sql = "UPDATE person SET userpw = ?, name= ?, age= ? WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, person.getUserPw()); //수정 자료를 DB에 userpw에 저장
+			pstmt.setString(2, person.getName());
+			pstmt.setInt(3, person.getAge());
+			pstmt.setString(4, person.getUserId());
+			pstmt.executeUpdate();	//db에 저장
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(conn, pstmt);
+		}
+	}
 	
 	
 	//자료 삭제
+	public void deletePerson(Person person) {
+		conn = JDBCUtil.getConnection();
+		String sql = "DELETE FROM person WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, person.getUserId()); //입력된 userid 저장
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(conn, pstmt);
+		}	
+	}
+	
+	// 1명 조회(상세보기)
+	public Person getPerson(String userId) {
+		Person person = new Person();	//새로운 객체 생성
+		conn = JDBCUtil.getConnection();
+		String sql = "SELECT * FROM person WHERE userid = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			rs = pstmt.executeQuery();	//검색된 자료 반환
+			if(rs.next()) {	//자료가 있으면
+				person.setUserId(rs.getString("userid"));	//db에 있는 userid 가져옴
+				person.setUserPw(rs.getString("userpw"));
+				person.setName(rs.getString("name"));
+				person.setAge(rs.getInt("age"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(conn, pstmt, rs);
+		}
+		
+		return person;
+	}
 	
 	
 }
--- jdbctest/src/dbconnection/PersonMain.java
+++ jdbctest/src/dbconnection/PersonMain.java
@@ -9,24 +9,50 @@ public class PersonMain {
 		PersonDAO dao = new PersonDAO();
 		
 		//자료 삽입
-		Person p1 = new Person();
-		p1.setUserId("today");
-		p1.setUserPw("today11");
-		p1.setName("투데이");
-		p1.setAge(15);
+		/*Person p1 = new Person();
+		p1.setUserId("gang");
+		p1.setUserPw("gang95");
+		p1.setName("원경");
+		p1.setAge(27);
 		
-		dao.insertPerson(p1);
+		dao.insertPerson(p1);*/
+		
+		//자료 수정
+		/*Person p2 = new Person();
+		p2.setUserId("gang");
+		p2.setUserPw("gang96");	//비밀번호 수정
+		p2.setName("원경");		//이름 수정
+		p2.setAge(27);
+		
+		dao.updatePerson(p2); 	//자료 수정 메서드 호출*/
+		
+		//자료 삭제
+		/*Person deleteOne = new Person();
+		deleteOne.setUserId("sky");
+		
+		dao.deletePerson(deleteOne);*/
+		
+		//특정한 1명 조회
+		Person selectOne = dao.getPerson("may");
+		printPerson(selectOne);
+		System.out.println("==================================");
 		
 		//전체 조회		
 		personList = dao.getPersonList();
 		for(int i = 0; i < personList.size(); i++) {
 			Person person = personList.get(i);
-			System.out.println("아이디: " + person.getUserId());
-			System.out.println("비밀번호: " + person.getUserPw());
-			System.out.println("이름: " + person.getName());
-			System.out.println("나이: " + person.getAge());
+			printPerson(person);
 		}
 		
+		
+		
+	}//main 닫기
+	
+	public static void printPerson(Person person) {
+		System.out.println("아이디: " + person.getUserId());
+		System.out.println("비밀번호: " + person.getUserPw());
+		System.out.println("이름: " + person.getName());
+		System.out.println("나이: " + person.getAge());
 	}
 
 }
