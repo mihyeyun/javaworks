2e8f54482a2f405216e74908ff85d2f957a7fbc4
mihyeyun <ymh___2761@naver.com>
2022-07-12 18:22:29 +0900

    ch08

---
+++ ch08/src/generic/printer/GenericPrinterTest.java
@@ -0,0 +1,17 @@
+package generic.printer;
+
+public class GenericPrinterTest {
+
+	public static void main(String[] args) {
+		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+		
+		plasticPrinter.setMaterial(new Plastic());
+		System.out.println(plasticPrinter);
+		
+		//Powder 클래스 사용
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+		powderPrinter.setMaterial(new Powder());
+		System.out.println(powderPrinter);
+	}
+
+}
